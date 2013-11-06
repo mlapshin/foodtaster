75a057afd247ae22669c2f5a3981464eac40dbda
Serzh Nechyporchuk <nechyporchuk@gmail.com>
2013-11-06 13:18:29 +0200

    fix service matcher

--- lib/foodtaster/rspec/matchers/service_matcher.rb
+++ lib/foodtaster/rspec/matchers/service_matcher.rb
@@ -1,15 +1,15 @@
 RSpec::Matchers.define :have_running_service do |service|
   match do |vm|
-    result = vm.execute("status #{service}").stdout
-    reuslt =~ /start\/running/
+    @result = vm.execute("status #{service}").stdout
+    @result =~ /start\/running/
   end
 
   failure_message_for_should do |vm|
-    "expected that #{vm.name} should have running service '#{service}'"
+    "expected that #{vm.name} should have running service '#{service}' but was '#{@result}'"
   end
 
   failure_message_for_should_not do |vm|
-    "expected that #{vm.name} should not have running service '#{service}'"
+    "expected that #{vm.name} should not have running service '#{service}' but was '#{@result}'"
   end
 
   description do
