07fc1c5a8c3d08c379cf82a11af1c6c5b17756a7
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-11-07 15:51:35 +0400

    run_chef_on block now accepts :rollback option instead of :skip_rollback

--- lib/foodtaster/rspec/dsl_methods.rb
+++ lib/foodtaster/rspec/dsl_methods.rb
@@ -11,11 +11,11 @@ module Foodtaster
 
       def run_chef_on(vm_name, options = {}, &block)
         require_vm(vm_name)
-        skip_rollback = Foodtaster.config.skip_rollback || options[:skip_rollback]
+        rollback = options.key?(:rollback) ? options[:rollback] : !Foodtaster.config.skip_rollback
 
         before(:all) do
           vm = get_vm(vm_name)
-          vm.rollback unless skip_rollback
+          vm.rollback if rollback
 
           run_chef_on(vm_name, &block)
         end
